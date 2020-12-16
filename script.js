821561e37fb5e6dff3235f5179501bba9fe3c6a2
subhajit patra <71601399+subhajitpatra498@users.noreply.github.com>
2020-12-16 21:10:50 +0530

    add

---
+++ script.js
@@ -0,0 +1,12 @@
+var prev=document.getElementById("prev");
+var next=document.getElementById("next");
+var thumbnail=document.getElementsByClassName("thumbnail");
+var hero=document.getElementById("hero");
+
+var backgroundImg=new Array(
+    "image/bg1.png",
+    "image/bg2.png",
+    "image/bg3.png",
+    "image/bg4.png",
+    "image/bg5.png"
+);
